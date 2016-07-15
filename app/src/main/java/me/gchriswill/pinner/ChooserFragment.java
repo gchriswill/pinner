6798bb10f2bc89c485439b94ef337e8afd385969
gchriswill <gchriswill@me.com>
2016-07-15 01:04:18 -0400

    Project now has options to login
    In this commit, I'm adding the activity and it's component that will contains the options for allowing the user to choose his preferred method between login using social networks and create account with email and password.

---
+++ app/src/main/java/me/gchriswill/pinner/ChooserFragment.java
@@ -0,0 +1,32 @@
+package me.gchriswill.pinner;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ChooserFragment extends Fragment {
+
+    ChooserFragmentInterface chooserFragmentInterface;
+
+    public ChooserFragment() {
+
+    }
+
+    public interface ChooserFragmentInterface {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_chooser, container, false);
+
+    }
+
+}
